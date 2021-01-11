e1af221b1debac69002628327fe49d2998dfaeb9
combateer3 <scottcorbat900@gmail.com>
2021-01-11 18:37:37 -0500

    initial commit

---
+++ src/test/java/com/scottcorbat/leagueapi/LeagueapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scottcorbat.leagueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
